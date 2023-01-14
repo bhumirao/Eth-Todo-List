d89a416aa785bd5f435d331427784aedb40f723c
Bhumi <bhumikarao1506@gmail.com>
2023-01-14 23:38:15 +0530

    deploying  debugged smart contract

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -1,26 +1,34 @@
 pragma solidity >=0.5.0;
 
+contract TodoList {
+    uint256 public taskCount = 0;
 
-contract TodoList{
-    uint public taskCount =0;
+    constructor() public {
+        createTask("Check out GitHub bhumirao !");
+    }
 
+    struct Task {
+        uint256 id;
+        string content;
+        bool completed;
+    }
 
-constructor() public {
-        createTask("Check out dappuniversity.com");
-      }
+    event TaskCompleted(uint256 id, bool completed);
 
+    mapping(uint256 => Task) public tasks;
 
-struct Task {
-    uint id;
-    string content;
-    bool completed;
-  }
+    event TaskCreated(uint256 id, string content, bool completed);
 
-  mapping(uint => Task) public tasks;
+    function createTask(string memory _content) public {
+        taskCount++;
+        tasks[taskCount] = Task(taskCount, _content, false);
+        emit TaskCreated(taskCount, _content, false);
+    }
 
-  function createTask(string memory _content) public {
-    taskCount ++;
-    tasks[taskCount] = Task(taskCount, _content, false);
-  }
-
+    function toggleCompleted(uint256 _id) public {
+        Task memory _task = tasks[_id];
+        _task.completed = !_task.completed;
+        tasks[_id] = _task;
+        emit TaskCompleted(_id, _task.completed);
+    }
 }
